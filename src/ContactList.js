7914987a66d0bf6d00f8bb987c26814a849e0a9d
rajan <rajangreedge@gmail.com>
2018-11-27 08:20:19 +0530

    Display contact avatar, name,email

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,8 +1,22 @@
 import React, {Component} from "react"
 class ListContacts extends Component {
 	render(){
-		console.log(this.props);
-		return (<ol className="contact-list"></ol>)
+		//console.log(this.props);
+		return (<ol className="contact-list">
+				{this.props.contacts.map(contact => (
+					<li key={contact.name} className="contact-list-item">
+						
+						<div className="contact-avatar" style={{
+							backgroundImage:`url(${contact.avatarURL})`
+						}}></div>
+						
+						<div className="contact-details">
+							<p>{contact.name}</p>
+							<p>{contact.email}</p>
+						</div>
+						<button className="contact-remove">Remove</button>
+					</li>))}
+			</ol>)
 	}
 }
 
